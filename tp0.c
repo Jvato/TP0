abd49efbca974a954114590e55f4e0f7d5f2e838
Joseph Valer Torres <jvaler@fi.uba.ar>
2022-03-27 00:23:39 -0300

    Primer Commit, archivos originales de tp0

---
+++ tp0.c
@@ -0,0 +1,23 @@
+#include "tp0.h"
+
+/* *****************************************************************
+ *                     FUNCIONES A COMPLETAR                       *
+ *         (ver en tp0.h la documentación de cada función)         *
+ * *****************************************************************/
+
+void swap(int *x, int *y) {
+}
+
+
+int maximo(int vector[], int n) {
+    return -1;
+}
+
+
+int comparar(int vector1[], int n1, int vector2[], int n2) {
+    return 0;
+}
+
+
+void seleccion(int vector[], int n) {
+}
